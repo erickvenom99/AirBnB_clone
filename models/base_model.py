203865490bbebcd3f60371985f282f4fd40f5dc5
erickvenom99 <prevenakhtar@mail.com>
2024-05-20 17:03:51 +0200

    add init file

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
 """
-Base Model
+  Base Model
 """
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
     """
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         method initialies the attribute
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
 
     def save(self):
         """
         Method update the update_at attribute in the base class
         """
+        models.storage.save()
         self.updated_at = datetime.utcnow()
 
     def __str__(self):
@@ -33,21 +42,7 @@ class BaseModel:
         """method returns the dictionary representation of an instance
         """
         att_dict = self.__dict__.copy()
-        att_dict["__class__"] = self.__class__.__name__
-        att_dict[" created_at"] = self.created_at.isoformat()
-        att_dict["updated_at"] = self.updated_at.isoformat()
+        att_dict["__class__"] = type(self).__name__
+        att_dict["created_at"] = att_dict["created_at"].isoformat()
+        att_dict["updated_at"] = att_dict["updated_at"].isoformat()
         return att_dict
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                       my_model_json[key]))
